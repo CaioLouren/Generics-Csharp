b2f096608690b3f3bed6b45bfc20bbafe4f7a148
Caio Lourenço <lourencocaio228@gmail.com>
2022-06-29 14:28:47 -0300

    Atividade generics

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Generics.Entities;
+using System;
+using System.Globalization;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+            PrintService<int> printService = new PrintService<int>();
+
+            for(int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());  
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+
+            Console.WriteLine();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
